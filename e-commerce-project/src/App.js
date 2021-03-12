c46035a0046af133935ffa55065667385f74c962
Alex <barankaslon@hotmail.com>
2021-03-12 01:26:37 -0400

    added header component

--- e-commerce-project/src/App.js
+++ e-commerce-project/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './pages/homepage/home-page';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 import './App.css';
 
@@ -9,6 +10,7 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
          <Route exact path='/' component={HomePage}/>
          <Route path='/shop' component={ShopPage}/>
