463866fcf6e9a841d6f6e40d5ce561a4d3ae6c17
Alex <barankaslon@hotmail.com>
2021-03-18 02:11:02 -0300

    setting up Redux

---
+++ e-commerce-project/src/redux/user/use.actions.js
@@ -0,0 +1,13 @@
+
+const setCurrentUser = (user) => {
+    return(
+        {
+            type: 'SET_CURRENT_USER',
+            payload: user
+        }
+
+    )
+
+}
+
+export default setCurrentUser;
